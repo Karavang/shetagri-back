f01523de7c48c8daa08c50e58653f328c8b7a46b
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-09-22 16:56:18 +0200

    in pic sended photo

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ const express = require("express");
 const { mongoConnect } = require("./forDb");
 const PORT = process.env.PORT || 3000;
 const app = express();
-const { default: mongoose } = require("mongoose");
-const TelegramBot = require("node-telegram-bot-api");
-const { Post } = require("./forDb");
+const logicBot = require("./logic");
 require("dotenv").config();
-const TOKEN = process.env.API_KEY_BOT;
-const bot = new TelegramBot(TOKEN, {
-  polling: true,
-});
 const startServer = async () => {
   await mongoConnect();
   app.listen(PORT, () => {
@@ -17,18 +11,3 @@ const startServer = async () => {
   });
 };
 startServer();
-
-// Логика бота
-bot.on("message", async (message) => {
-  console.log(message);
-  const body = {
-    text: message.text || message.caption,
-    pic: message.photo,
-  };
-  console.log(body);
-  const text = await Post.create(body);
-  console.log(text);
-  bot.sendMessage(message.from.id, "Пост добавлен");
-});
-
-// Запуск сервера
---
+++ logic.js
@@ -0,0 +1,40 @@
+const TelegramBot = require("node-telegram-bot-api");
+const { Post } = require("./forDb");
+require("dotenv").config();
+const TOKEN = process.env.API_KEY_BOT;
+
+const bot = new TelegramBot(TOKEN, {
+  polling: true,
+});
+
+const logicBot = bot.on("message", async (message) => {
+  try {
+    if (message.photo) {
+      // Find the maximum resolution photo
+      const maxPhoto = message.photo.reduce((prev, current) => {
+        if (current.width > prev.width) {
+          return current;
+        }
+        return prev;
+      });
+
+      const fileData = await bot.getFile(maxPhoto.file_id);
+
+      const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${fileData.file_path}`;
+
+      const body = {
+        text: message.text || message.caption,
+        pic: imageUrl,
+      };
+
+      console.log(body);
+      const text = await Post.create(body);
+      console.log(text);
+      bot.sendMessage(message.from.id, "Пост добавлен");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = logicBot;
