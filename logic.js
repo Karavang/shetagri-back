431d7239b0cb03369790027140a90e2a037f3ccd
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2023-09-22 22:22:58 +0200

    Now you can add post without picture

--- logic.js
+++ logic.js
@@ -9,23 +9,21 @@ const bot = new TelegramBot(TOKEN, {
 
 const logicBot = bot.on("message", async (message) => {
   try {
-    if (message.photo) {
-      // Find the maximum resolution photo
-      const maxPhoto = message.photo.reduce((prev, current) => {
-        if (current.width > prev.width) {
-          return current;
-        }
-        return prev;
-      });
-
-      const fileData = await bot.getFile(maxPhoto.file_id);
-
-      const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${fileData.file_path}`;
-
+    if (message) {
       const body = {
         text: message.text || message.caption,
-        pic: imageUrl,
       };
+      if (message.photo) {
+        const maxPhoto = message.photo.reduce((prev, current) => {
+          if (current.width > prev.width) {
+            return current;
+          }
+          return prev;
+        });
+        const fileData = await bot.getFile(maxPhoto.file_id);
+        const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${fileData.file_path}`;
+        body.pic = imageUrl;
+      }
 
       console.log(body);
       const text = await Post.create(body);
