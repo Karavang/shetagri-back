e091451531e6ce2507a9ca9ac70ab1eddc357073
Karavang <k4r4v4ng@gmail.com>
2023-10-18 17:03:10 +0200

    tests#6

--- postPost.js
+++ postPost.js
@@ -1,7 +1,7 @@
 const { Post } = require("./forDb");
 const postPost = async (req, res) => {
   try {
-    res.status(200).json(req.body);
+    res.status(200).json(req);
   } catch (error) {
     res
       .status(400)
