39093d48bd9f644a699cf2b2e1d5fa5620ddab14
Karavang <k4r4v4ng@gmail.com>
2023-10-18 17:17:00 +0200

    little fixed

--- postPost.js
+++ postPost.js
@@ -1,7 +1,8 @@
 const { Post } = require("./forDb");
 const postPost = async (req, res) => {
   try {
-    res.status(200).json(req);
+    Post.create(req.body);
+    res.status(200).json(req.body);
   } catch (error) {
     res
       .status(400)
