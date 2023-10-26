376240164022b6e13b8e7d7206b29420058e6eaf
Karavang <k4r4v4ng@gmail.com>
2023-10-26 19:02:50 +0200

    by chatGPT

--- logic.js
+++ logic.js
@@ -1,39 +1,49 @@
 const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
 const { Post } = require("./forDb");
-const postPost = require("./postPost");
-const { default: axios } = require("axios");
 require("dotenv").config();
+
 const TOKEN = process.env.API_KEY_BOT;
+const postUrl = "https://shetagri-back.vercel.app/";
 
 const bot = new TelegramBot(TOKEN, {
   polling: true,
 });
-const postUrl = "https://shetagri-back.vercel.app/";
-const logicBot = bot.on("message", async (message) => {
-  try {
-    if (message) {
+
+const getMaxPhoto = (photos) => {
+  return photos.reduce((prev, current) =>
+    current.width > prev.width ? current : prev,
+  );
+};
+
+const processMessage = async (message) => {
+  if (message) {
+    try {
       const body = {
         text: message.text || message.caption,
       };
+
       if (message.photo) {
-        const maxPhoto = message.photo.reduce((prev, current) => {
-          if (current.width > prev.width) {
-            return current;
-          }
-          return prev;
-        });
+        const maxPhoto = getMaxPhoto(message.photo);
         const fileData = await bot.getFile(maxPhoto.file_id);
         const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${fileData.file_path}`;
         body.pic = imageUrl;
       }
-      console.log(body);
+
       await axios.post(postUrl, body);
       await Post.create(body);
-      await bot.sendMessage(message.from.id, "Пост добавлен");
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
 
-module.exports = logicBot;
+      await bot.sendMessage(message.chat.id, "Пост добавлен");
+    } catch (error) {
+      console.error(error);
+      await bot.sendMessage(
+        message.chat.id,
+        "Произошла ошибка при добавлении поста",
+      );
+    }
+  }
+};
+
+bot.on("message", processMessage);
+
+module.exports = processMessage;
