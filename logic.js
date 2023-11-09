38538c89f808b420d248ad056795fe32286d840c
Karavang <k4r4v4ng@gmail.com>
2023-11-10 00:23:38 +0100

    back

--- logic.js
+++ logic.js
@@ -22,9 +22,9 @@ bot.on("message", async (message) => {
           return prev;
         });
         const fileData = await bot.getFile(maxPhoto.file_id);
-        const fileBuffer = await bot.downloadFile(fileData.file_path);
+
         const imageUrl = `https://api.telegram.org/file/bot${TOKEN}/${fileData.file_path}`;
-        body.pic = fileBuffer;
+        body.pic = imageUrl;
       }
       console.log(body);
       await Post.create(body);
