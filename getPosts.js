4eb4bb2e4a1a0b37dcacf0875e9eefc578af7ad9
Karavang <k4r4v4ng@gmail.com>
2024-12-14 17:03:41 +0100

    get back

--- getPosts.js
+++ getPosts.js
@@ -3,8 +3,7 @@ const getPosts = async (req, res) => {
   try {
     const posts = await Post.find();
 
-    const revertedPosts = posts.reverse();
-    res.status(200).json(revertedPosts);
+    res.status(200).json(posts);
   } catch (error) {
     res
       .status(400)
